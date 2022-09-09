d8b9804863df41c8fef9bea98518175fd1865525
caldwmark <caldwmark@gmail.com>
2022-09-09 14:29:34 -0800

    removed console.log

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,8 +1,6 @@
 import Link from 'next/link'
 
 const Post = ({ post }) => {
-    console.log(post)
-
     const date = new Date(post.publishedAt).toLocaleDateString('en-us', {
         weekday: 'short',
         year: 'numeric',
@@ -16,7 +14,6 @@ const Post = ({ post }) => {
                 <img src={post.mainImage} alt='' />
                 <h3>{post.title}</h3>
                 <div className='post-date'>{date}</div>
-                {/* <p>{post.frontmatter.excerpt}</p> */}
             </div>
         </Link>
     )
