7bfedf18e64286f056552f50f6ff0fae8de2f08c
caldwmark <caldwmark@gmail.com>
2022-09-14 11:44:56 -0800

    added: modular style

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,23 +1,45 @@
 import Link from 'next/link'
+import style from '../styles/Header.module.css'
 
 export default function Header() {
     return (
-        <header className='main-header'>
-            <div className='container flex-nav'>
-                <nav className='header-layout'>
-                    <Link href='/'>
-                        <h2 className='header-logo'>Bear Country Publishing</h2>
+        <header className={style[`main-header`]}>
+            <div className={style[`flex-nav`]}>
+                <nav className={style[`header-layout`]}>
+                    <Link className={style[`main-header-link`]} href='/'>
+                        <h2 className={style[`header-logo`]}>
+                            Bear Country Publishing
+                        </h2>
                     </Link>
-                    <div className='main-nav'>
-                        <ul className='main-nav-list'>
-                            <li>
-                                <Link href='/'>Home</Link>
+                    <div className={style[`main-nav`]}>
+                        <ul className={style[`main-nav-list`]}>
+                            <li className={style[`nav-list-item`]}>
+                                <Link
+                                    className={style[`main-nav-link`]}
+                                    href='/'
+                                >
+                                    <a className={style['link-anchor']}>Home</a>
+                                </Link>
                             </li>
-                            <li>
-                                <Link href='/documents'>Documents</Link>
+                            <li className={style[`nav-list-item`]}>
+                                <Link
+                                    className={style[`main-nav-link`]}
+                                    href='/documents'
+                                >
+                                    <a className={style['link-anchor']}>
+                                        Documents
+                                    </a>
+                                </Link>
                             </li>
-                            <li>
-                                <Link href='/about'>About</Link>
+                            <li className={style[`nav-list-item`]}>
+                                <Link
+                                    className={style[`main-nav-link`]}
+                                    href='/about'
+                                >
+                                    <a className={style['link-anchor']}>
+                                        About
+                                    </a>
+                                </Link>
                             </li>
                         </ul>
                     </div>
