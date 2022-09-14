c22a47e5d4ae954cde334e26c0bf28628a3b1fcc
caldwmark <caldwmark@gmail.com>
2022-09-13 19:04:59 -0800

    content: added links to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,9 +2,15 @@ const YEAR = new Date().getFullYear()
 
 const Footer = () => {
     return (
-        <footer>
-            <div className='container'>
-                &copy; {YEAR} by Bear Country Publishing
+        <footer className=''>
+            <div className='flex-footer container'>
+                &copy; {YEAR} &middot; Bear Country Publishing
+                <a href='https://nextjs.org/'>Next</a>
+                <a href='https://reactjs.org/'>React</a>
+                <a href='https://www.sanity.io/'>Sanity</a>
+                <a href='https://github.com/caldwmark/next-sanity-blog'>
+                    GitHub
+                </a>
             </div>
         </footer>
     )
