47485c24523213146f6857cfeb4a83240d5fa384
caldwmark <caldwmark@gmail.com>
2022-09-14 17:33:39 -0800

    lighthouse: footer link sizes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,13 +5,25 @@ const Footer = () => {
         <footer className=''>
             <div className='flex-footer container'>
                 &copy; {YEAR} &middot; Bear Country Publishing
-                <a href='https://nextjs.org/'>Next</a>
-                <a href='https://reactjs.org/'>React</a>
-                <a href='https://www.sanity.io/'>Sanity</a>
-                <a href='https://web.dev/measure/?gclid=CjwKCAjw6fyXBhBgEiwAhhiZsomBRG6o3zyMvSZGg-MWoDmu2N2J25HYLMBI9DSUvRZUmHrZ13oLdxoC1v4QAvD_BwE&url=https%3A%2F%2Fwww.bearcountrypublishing.com'>
+                <a className='footer-link' href='https://nextjs.org/'>
+                    Next
+                </a>
+                <a className='footer-link' href='https://reactjs.org/'>
+                    React
+                </a>
+                <a className='footer-link' href='https://www.sanity.io/'>
+                    Sanity
+                </a>
+                <a
+                    className='footer-link'
+                    href='https://web.dev/measure/?gclid=CjwKCAjw6fyXBhBgEiwAhhiZsomBRG6o3zyMvSZGg-MWoDmu2N2J25HYLMBI9DSUvRZUmHrZ13oLdxoC1v4QAvD_BwE&url=https%3A%2F%2Fwww.bearcountrypublishing.com'
+                >
                     Lighthouse
                 </a>
-                <a href='https://github.com/caldwmark/next-sanity-blog'>
+                <a
+                    className='footer-link'
+                    href='https://github.com/caldwmark/next-sanity-blog'
+                >
                     GitHub
                 </a>
             </div>
