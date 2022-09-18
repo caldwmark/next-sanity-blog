b0d68e52362a712ecdf50f073797dfaacd1ce5bb
caldwmark <caldwmark@gmail.com>
2022-09-18 01:36:30 -0800

    added: schema for pages - about documents, etc

---
+++ studio/schemas/about.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'page',
+    title: 'Page',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'body',
+            title: 'Body',
+            type: 'blockContent',
+        },
+    ],
+}
