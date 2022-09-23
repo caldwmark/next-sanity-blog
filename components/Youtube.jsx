b30c006f7679bb33a3a0f1008a06506cad3868d3
caldwmark <caldwmark@gmail.com>
2022-09-23 04:09:54 -0800

    changed: minor logic

--- components/Youtube.jsx
+++ components/Youtube.jsx
@@ -1,13 +1,15 @@
 import style from '../styles/Post.module.css'
 
 const Youtube = ({ embedCode }) => {
+    const source = `https://www.youtube.com/embed/${embedCode}`
+
     return (
         <div className={style.responsive}>
             <iframe
                 className={style.video}
                 width='800'
                 height='450'
-                src={`https://www.youtube.com/embed/${embedCode}`}
+                src={source}
                 title='YouTube video player'
                 frameBorder='0'
                 allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
