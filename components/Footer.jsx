0d174ca98bb7bbd6e4513f42d35a7bbe838d1916
caldwmark <caldwmark@gmail.com>
2022-09-26 01:03:11 -0800

    layout: to columns

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,36 +5,31 @@ const YEAR = new Date().getFullYear()
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <div className={styles[`flex-footer`]}>
-                &copy;{YEAR} &middot; Bear Country Publishing
-                <a className={styles[`footer-link`]} href='https://nextjs.org/'>
-                    Next
-                </a>
-                <a
-                    className={styles[`footer-link`]}
-                    href='https://reactjs.org/'
-                >
-                    React
-                </a>
-                <a
-                    className={styles[`footer-link`]}
-                    href='https://www.sanity.io/'
-                >
-                    Sanity
-                </a>
-                <a
-                    className={styles[`footer-link`]}
-                    href='https://web.dev/measure/?gclid=CjwKCAjw6fyXBhBgEiwAhhiZsomBRG6o3zyMvSZGg-MWoDmu2N2J25HYLMBI9DSUvRZUmHrZ13oLdxoC1v4QAvD_BwE&url=https%3A%2F%2Fwww.bearcountrypublishing.com'
-                >
-                    Lighthouse
-                </a>
-                <a
-                    className={styles[`footer-link`]}
-                    href='https://github.com/caldwmark/next-sanity-blog'
-                >
-                    GitHub
-                </a>
-            </div>
+            <section className={styles.column}>
+                <p className={styles.copy}>
+                    &copy;{YEAR} &middot; Bear Country Publishing
+                </p>
+            </section>
+            <section className={styles.column}>
+                <ul className={styles.list}>
+                    <li className={styles.item}>
+                        <a
+                            className={styles[`footer-link`]}
+                            href='https://web.dev/measure/?gclid=CjwKCAjw6fyXBhBgEiwAhhiZsomBRG6o3zyMvSZGg-MWoDmu2N2J25HYLMBI9DSUvRZUmHrZ13oLdxoC1v4QAvD_BwE&url=https%3A%2F%2Fwww.bearcountrypublishing.com'
+                        >
+                            Check Lighthouse Scores
+                        </a>
+                    </li>
+                    <li className={styles.item}>
+                        <a
+                            className={styles[`footer-link`]}
+                            href='https://github.com/caldwmark/next-sanity-blog'
+                        >
+                            This Site on GitHub
+                        </a>
+                    </li>
+                </ul>
+            </section>
         </footer>
     )
 }
