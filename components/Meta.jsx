92a11dc4135f6b08edaae24ec2be35dfea0b20aa
caldwmark <caldwmark@gmail.com>
2022-09-26 22:22:40 -0800

    added: new favicon files

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -39,6 +39,24 @@ const Meta = ({ title, desc, robots, image, ogPage, slug }) => {
             <meta property='og:image:width' content='420' />
             <meta property='og:image:height' content='75' />
             <meta property='og:image:alt' content='video thumbnail' />
+            <link
+                rel='apple-touch-icon'
+                sizes='180x180'
+                href='/apple-touch-icon.png'
+            />
+            <link
+                rel='icon'
+                type='image/png'
+                sizes='32x32'
+                href='/favicon-32x32.png'
+            />
+            <link
+                rel='icon'
+                type='image/png'
+                sizes='16x16'
+                href='/favicon-16x16.png'
+            />
+            <link rel='manifest' href='/site.webmanifest' />
             <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
         </Head>
     )
