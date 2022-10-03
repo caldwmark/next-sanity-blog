c4d43f73609c6a9f5c0b96b8b35609761ea4a9e3
caldwmark <caldwmark@gmail.com>
2022-10-02 16:09:44 -0800

    layout: mobile style and others

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,13 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
     return (
-        <>
+        <div className='wrapper'>
             <Header />
             <main className='container'>
                 <Component {...pageProps} />
             </main>
             <Footer />
-        </>
+        </div>
     )
 }
 
